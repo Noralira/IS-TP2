1bd765814999a69a95c8fd485cec2c15ed15e62b
Noralira <96257729+Noralira@users.noreply.github.com>
2023-01-23 00:41:05 +0000

    -

--- src/frontend/ent/src/Layout/Sections.js
+++ src/frontend/ent/src/Layout/Sections.js
@@ -3,7 +3,6 @@ import Flights from "../Tables/flights";
 import Classes from "../Tables/classes";
 import Airlines from "../Tables/airlines";
 import Routes from "../Tables/routes";
-import Times_flights from "../Tables/times_flights";
 import Times from "../Tables/times";
 
 
@@ -30,11 +29,6 @@ const Sections = [
         id: "routes",
         label: "Routes",
         content: <Routes/>
-    },
-    {
-        id: "times_flights",
-        label: "Times_flights",
-        content: <Times_flights/>
     },
     {
         id: "times",
